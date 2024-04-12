7992e8b08799b267f7d28742e253b7571255a9e0
Kim YongJin <88384221+Kaijin45@users.noreply.github.com>
2024-04-12 09:29:16 +0900

    [Silver IV] Title: 스택 2, Time: 756 ms, Memory: 132364 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.java
+++ 백준/Silver/28278. 스택 2/스택 2.java
@@ -1,41 +1,42 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.StringTokenizer;
+import java.util.Stack;
 
-public class Main{
+public class Main {
     
-    static LinkedList<Integer> stack = new LinkedList<>();
+    static Stack<Integer> stack = new Stack<>();
+    static StringBuilder sb = new StringBuilder();
     
-    public static void main(String[] args) throws IOException{
-        BufferedReader br =
-            new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
-        StringTokenizer st;
-        
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(br.readLine()); // 명령어의 수
         
         while(N --> 0){
-            st = new StringTokenizer(br.readLine());
-            String command = st.nextToken();
-            if(command.equals("1")){
-                stack.addFirst(Integer.parseInt(st.nextToken()));
-            }
-            else if(command.equals("2")){
-                sb.append(stack.isEmpty() ? -1 : stack.pollFirst()).append("\n");
-            }
-            else if(command.equals("3")){
-                sb.append(stack.size()).append("\n");
-            }
-            else if(command.equals("4")){
-                sb.append(stack.isEmpty() ? 1 : 0).append("\n");
-            }
-            else if(command.equals("5")){
-                sb.append(stack.isEmpty() ? -1 : stack.getFirst()).append("\n");
-            }
+            solution(br.readLine());
         }
+        
         br.close();
         
         System.out.println(sb);
+    }
+    
+    static void solution(String query){
+        char c = query.charAt(0); // 첫번째 문자가 명령어
+        
+        switch(c){
+            // query.substring(2)의 의미는 공백도 문자로 포함되기 때문에. 1 X 형태이기에 X의 인덱스 2 사용
+            case '1' :
+                stack.push(Integer.parseInt(query.substring(2))); return;
+            case '2' :
+                sb.append(stack.isEmpty() ? -1 : stack.pop()).append("\n"); return;
+            case '3' :
+                sb.append(stack.size()).append("\n"); return;
+            case '4' :
+                sb.append(stack.isEmpty() ? 1 : 0).append("\n"); return;
+            case '5' :
+                sb.append(stack.isEmpty() ? -1 : stack.peek()).append("\n"); return;
+            default :
+                break;
+        }
     }
 }
