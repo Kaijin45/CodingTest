2cace2dd1e549476a359b1e2f57cb10942024e22
Kim YongJin <88384221+Kaijin45@users.noreply.github.com>
2025-05-02 23:08:55 +0900

    [Silver V] Title: 멘토와 멘티, Time: 364 ms, Memory: 56264 KB -BaekjoonHub

--- 백준/Silver/26265. 멘토와 멘티/멘토와 멘티.py
+++ 백준/Silver/26265. 멘토와 멘티/멘토와 멘티.py
@@ -6,18 +6,8 @@ testcase = int(input().strip())
 for _ in range(testcase):
     mmlist.append(list(input().split()))
     
+mmlist.sort(key=lambda x: x[1], reverse=True)
 mmlist.sort(key=lambda x: x[0])
-mmdic = {}
 
-for i in mmlist:
-    if i[0] in mmdic.keys():
-        mmdic[i[0]].append(i[1])
-    else:
-        mmdic[i[0]] = [i[1]]
-        
-for i in mmdic:
-    mmdic[i].sort(reverse = True)
-    
-for i in mmdic:
-    for j in mmdic[i]:
-        print(i, j)
+for i in range(testcase):
+    print(mmlist[i][0], mmlist[i][1])
