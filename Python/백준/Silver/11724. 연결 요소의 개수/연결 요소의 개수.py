ff38ec9c6db252a0769ca3b8fcd644f8c9fffdf7
Kim YongJin <88384221+Kaijin45@users.noreply.github.com>
2025-05-08 22:35:41 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 640 ms, Memory: 67064 KB -BaekjoonHub

--- Python/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ Python/백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -4,8 +4,7 @@ from collections import deque
 
 node, edge = map(int, input().split())
 adj_list = [[] for _ in range(node+1)]
-visited = [False] * (node+1)
-connected_component = 0
+visited = [0] * (node+1)
 
 for _ in range(edge):
     x, y = map(int, input().split())
@@ -14,18 +13,18 @@ for _ in range(edge):
 
 def BFS(g, v, visit):
     queue = deque([v])
-    visit[v] = True
+    visit[v] = visit[0]
     
     while queue:
         vv = queue.popleft()
         for i in g[vv]:
-            if not visit[i]:
+            if visit[i] == 0:
                 queue.append(i)
-                visit[i] = True
+                visit[i] = visit[0]
                 
 for i in range(1, node+1):
-    if not visited[i]:
-        connected_component += 1
+    if visited[i] == 0:
+        visited[0] += 1
         BFS(adj_list, i, visited)
 
-print(connected_component)
+print(visited[0])
